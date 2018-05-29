a8288aebec52243ba79f9dc93c922e9cce2ab01f
work <king@work>
2018-05-30 00:21:45 +0300

    added Migrations; added Thing model and context

---
+++ sklad/Models/Thing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sklad.Models
+{
+    public class Thing
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }
+        public DateTime ProduceDate { get; set; }
+    }
+}
---
+++ sklad/Models/WarehouseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace sklad.Models
+{
+    public class WarehouseContext : DbContext
+    {
+        public DbSet<Thing> Things { get; set; }
+    }
+}
